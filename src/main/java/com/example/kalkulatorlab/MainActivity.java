e7159324c8e6900380550e648c8efbe366822d76
RayzenOri <raizenori@gmail.com>
2023-05-16 20:27:55 +0200

    Horizontal

--- src/main/java/com/example/kalkulatorlab/MainActivity.java
+++ src/main/java/com/example/kalkulatorlab/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
